088e57642341fe32c50afcca7ec8c9e3ec39753d
MatheusPortal <matheusgamer714@gmail.com>
2026-04-30 21:28:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Formadora02',
+  webDir: 'www'
+};
+
+export default config;
